8679a030691b7b78d778bfd22aecf57b35346ca0
GreatSage <PiotrDziura@gmail.com>
2017-09-17 15:14:15 +0200

    BaseEntity

---
+++ PartyTime/PartyTime.Core/Domain/BaseEntity.cs
@@ -0,0 +1,9 @@
+using System;
+
+public class BaseEntity
+{
+    public Int64 Id { get; set; }
+    public DateTime AddedDate { get; set; }
+    public DateTime ModifiedDate { get; set; }
+    public string IPAddress { get; set; }
+}
